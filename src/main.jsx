68e8335b4b0c45a49a3eb89afc680b0da0280ea5
murtazajoo <jmtariq2003@gmail.com>
2025-08-15 18:20:40 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <Routes>
+        <Route path="/" element={<>
+          <h1>Welcome to the Photo Slideshow App</h1>
+          <p>Select a theme and mode to get started.</p>
+          <NavLink to="/07_module_e">Go to Slideshow</NavLink>
+        </>} />
       <Route path="/07_module_e" element={<App />} />
     </Routes>
     </BrowserRouter>
